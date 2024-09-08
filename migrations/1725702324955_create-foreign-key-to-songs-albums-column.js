90a44bcfc3c5955fcae717493afaf31b48af71f4
Julian <julianchandrasutadi@gmail.com>
2024-09-08 15:20:32 +0700

    feat: new db migrations

---
+++ migrations/1725702324955_create-foreign-key-to-songs-albums-column.js
@@ -0,0 +1,26 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+exports.shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.up = (pgm) => {
+  pgm.sql("INSERT INTO albums(id, name, year) VALUES ('old_songs', 'old_songs', 0)");
+  pgm.sql("UPDATE songs SET album_id = 'old_songs' WHERE album_id IS NULL");
+  pgm.addConstraint('songs', 'fk_songs.album_id_albums.id', 'FOREIGN KEY(album_id) REFERENCES albums(id) ON DELETE CASCADE');
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.down = (pgm) => {
+  pgm.dropConstraint('songs', 'fk_songs.album_id_albums.id');
+  pgm.sql("UPDATE songs SET album_id = NULL WHERE album_id = 'old_songs'");
+  pgm.sql("DELETE FROM albums WHERE id = 'old_songs'");
+};
